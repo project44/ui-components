f50e46333f33b36b44313e918e30326b121186a7
Rob <robabby23@gmail.com>
2018-09-06 12:31:18 -0500

    Ant Design

--- example/src/App.js
+++ example/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 
-import { Foo, Bar } from 'ui-components'
+import { Foo, Bar } from 'ui-components';
+
+import { Button } from 'antd';
 
 export default class App extends Component {
   render () {
     return (
       <div>
+        <Button type="primary">Primary</Button>
         <Foo text='Foo Props' />
         <Bar text='Bar Props' />
       </div>
