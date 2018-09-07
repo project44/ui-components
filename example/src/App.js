7ef9de508e2c3a139b9c69c9f74f24d5719355ea
Rob <robabby23@gmail.com>
2018-09-07 08:51:33 -0500

    Ant WIP

--- example/src/App.js
+++ example/src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import { Foo, Bar } from 'ui-components';
 
 import { Button } from 'antd';
 
+import Header from './components/common/Header';
+import SideNav from './components/common/SideNav';
+
 export default class App extends Component {
   render () {
     return (
       <div>
-        <Button type="primary">Primary</Button>
-        <Foo text='Foo Props' />
-        <Bar text='Bar Props' />
+        <Header />
+        <SideNav />
       </div>
     )
   }
