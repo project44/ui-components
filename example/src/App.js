f8c05c8e378a4cccba19b6c2980d62850585d861
Rob <robabby23@gmail.com>
2018-09-07 11:10:02 -0500

    baseName

--- example/src/App.js
+++ example/src/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import { Foo, Bar } from 'ui-components';
+// import { Foo, Bar } from 'ui-components';
 
-import { Button } from 'antd';
-
-import Header from './components/common/Header';
+// import Header from './components/common/Header';
 import SideNav from './components/common/SideNav';
 import Components from './components/common/Components';
+import ComponentShell from './components/common/ComponentShell';
 import Landing from './components/Landing';
 
 
-export default class App extends Component {
+class App extends Component {
   render () {
     console.log(this.props);
     return (
-      <BrowserRouter>
+      <BrowserRouter  basename="/ui-components">
         <div className="p44-ui">
           {/* <Header /> */}
           <SideNav />
           <Route exact path="/" component={Landing} />
           <Route exact path="/components" component={Components} />
+          <Route exact path="/components/foo" component={ComponentShell} />
         </div>
       </BrowserRouter>
     )
   }
 }
+
+export default App;
