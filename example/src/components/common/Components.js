07dbcecafa01eb04b0477c7c7cec141a363d5cbe
Rob <robabby23@gmail.com>
2018-09-13 12:36:29 -0500

    Fix

--- example/src/components/common/Components.js
+++ example/src/components/common/Components.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import Foo from '../components/Foo';
-import Bar from '../components/Bar';
 import Buttons from '../components/Buttons';
 import DatePicker from '../components/DatePicker'
 import ChipsDemo from '../components/Chips'
@@ -13,7 +12,6 @@ export default (props) => {
   return (
     <div className="p44-ui__component">
       <Route exact path={`${match.url}/foo`} component={Foo} />
-      <Route exact path={`${match.url}/bar`} component={Bar} />
       <Route exact path={`${match.url}/buttons`} component={Buttons} />
       <Route exact path={`${match.url}/date-picker`} component={DatePicker} />
       <Route exact path={`${match.url}/chips`} component={ChipsDemo} />
