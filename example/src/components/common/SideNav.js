65c9ac55a8fabd1aabaa97c1ff80d9e8b7748a47
vije8001 <Jerry.Virgo.consultant@nielsen.com>
2018-10-20 15:23:44 -0500

    updating build again

--- example/src/components/common/SideNav.js
+++ example/src/components/common/SideNav.js
@@ -17,79 +17,79 @@ class SideNav extends Component {
 
   render() {
     return (
-        <Menu
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          mode="inline"
-          // theme="dark"
-          inlineCollapsed={this.state.collapsed}
-        >
-          <Menu.Item key="1">
-            <Link to={'/'}>
-              <Icon type="book" />
-              <span>About</span>
+      <Menu
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        mode='inline'
+        // theme='dark'
+        inlineCollapsed={this.state.collapsed}
+      >
+        <Menu.Item key='1'>
+          <Link to={'/'}>
+            <Icon type='book' />
+            <span>About</span>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key='2'>
+          <Link to={'/colors'}>
+            <Icon type='bg-colors' />
+            <span>Colors</span>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key='3'>
+          <Link to={'/typography'}>
+            <Icon type='font-size' />
+            <span>Typography</span>
+          </Link>
+        </Menu.Item>
+        <SubMenu key='sub1' title={<span><Icon type='layout' /><span>Components</span></span>}>
+          <Menu.Item key='7'>
+            <Link to={'/components/buttons'}>
+              Buttons
             </Link>
           </Menu.Item>
-          <Menu.Item key="2">
-            <Link to={'/colors'}>
-              <Icon type="bg-colors" />
-              <span>Colors</span>
+          <Menu.Item key='8'>
+            <Link to={'/components/date-picker'}>
+              Date Picker
             </Link>
           </Menu.Item>
-          <Menu.Item key="3">
-            <Link to={'/typography'}>
-              <Icon type="font-size" />
-              <span>Typography</span>
+          <Menu.Item key='9'>
+            <Link to={'/components/chips'}>
+              Chips
             </Link>
           </Menu.Item>
-          <SubMenu key="sub1" title={<span><Icon type="layout" /><span>Components</span></span>}>
-            <Menu.Item key="7">
-              <Link to={'/components/buttons'}>
-                Buttons
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="8">
-              <Link to={'/components/date-picker'}>
-                Date Picker
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="9">
-              <Link to={'/components/chips'}>
-                Chips
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="10">
-              <Link to={'/components/input'}>
-                Input
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="11">
-              <Link to={'/components/checkbox'}>
-                Checkbox
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="13">
-              <Link to={'/components/select'}>
-                Select
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="14">
-              <Link to={'/components/radio'}>
-                Radio
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="15">
-              <Link to={'/components/shipment-mode-icon'}>
-                Shipment Mode Icon
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="16">
-              <Link to={'/components/sub-header'}>
-                Sub-Header
-              </Link>
-            </Menu.Item>
-          </SubMenu>
-        </Menu>
+          <Menu.Item key='10'>
+            <Link to={'/components/input'}>
+              Input
+            </Link>
+          </Menu.Item>
+          <Menu.Item key='11'>
+            <Link to={'/components/checkbox'}>
+              Checkbox
+            </Link>
+          </Menu.Item>
+          <Menu.Item key='13'>
+            <Link to={'/components/select'}>
+              Select
+            </Link>
+          </Menu.Item>
+          <Menu.Item key='14'>
+            <Link to={'/components/radio'}>
+              Radio
+            </Link>
+          </Menu.Item>
+          <Menu.Item key='15'>
+            <Link to={'/components/shipment-mode-icon'}>
+              Shipment Mode Icon
+            </Link>
+          </Menu.Item>
+          <Menu.Item key='16'>
+            <Link to={'/components/sub-header'}>
+              Sub-Header
+            </Link>
+          </Menu.Item>
+        </SubMenu>
+      </Menu>
     );
   }
 }
