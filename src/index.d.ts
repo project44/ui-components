9ee6765cd4747b15c1fbf8df0a0550cdf91e63ec
Adam Nolte <anolte@p-44.com>
2019-01-21 11:39:13 -0600

    Fix Typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -23,12 +23,12 @@ declare module 'ui-components' {
     className?: string;
   }
 
-  export const Button: (props: ButtonProps) => React.FunctionComponent<ButtonProps>;
-  export const ColorPicker: (props: ColorPickerProps) => React.Component<ColorPickerProps, any>
-  export const Layers: (props: IconProps) => React.FunctionComponent<IconProps>;
-  export const Settings: (props: IconProps) => React.FunctionComponent<IconProps>;
-  export const Checkmark: (props: IconProps) => React.FunctionComponent<IconProps>;
-  export const Close: (props: IconProps) => React.FunctionComponent<IconProps>;
-  export const Info: (props: IconProps) => React.FunctionComponent<IconProps>;
-  export const ChevronDown: (props: IconProps) => React.FunctionComponent<IconProps>;
+  export const Button: (props: ButtonProps) => React.FunctionComponent<ButtonProps, {}>;
+  export class ColorPicker extends React.Component<ColorPickerProps, any> {};
+  export const Layers: (props: IconProps) => React.FunctionComponent<IconProps, {}>;
+  export const Settings: (props: IconProps) => React.FunctionComponent<IconProps, {}>;
+  export const Checkmark: (props: IconProps) => React.FunctionComponent<IconProps, {}>;
+  export const Close: (props: IconProps) => React.FunctionComponent<IconProps, {}>;
+  export const Info: (props: IconProps) => React.FunctionComponent<IconProps, {}>;
+  export const ChevronDown: (props: IconProps) => React.FunctionComponent<IconProps, {}>;
 }
