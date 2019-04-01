b1e2c03cfcf388cf9b3d4cecab410801d46f6094
Adam Nolte <anolte@project44.com>
2019-04-01 13:31:01 -0500

    Build Library

--- dist/index.js
+++ dist/index.js
@@ -26270,7 +26270,7 @@ var store = _global[SHARED] || (_global[SHARED] = {});
   return store[key] || (store[key] = value !== undefined ? value : {});
 })('versions', []).push({
   version: _core.version,
-  mode: _library ? 'pure' : 'global',
+  mode: 'pure',
   copyright: '© 2019 Denis Pushkarev (zloirock.ru)'
 });
 });
