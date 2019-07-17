99afbdbe8b449ad08821916889b748756afef15b
Adam Nolte <anolte@project44.com>
2019-07-17 15:00:48 -0500

    Build New Version

--- dist/index.es.js
+++ dist/index.es.js
@@ -35595,6 +35595,50 @@ NoteIcon.propTypes = {
   className: PropTypes.string
 };
 
+var BookIcon = function BookIcon(_ref) {
+  var className = _ref.className;
+
+  return React.createElement(
+    'svg',
+    {
+      className: className,
+      width: '24px',
+      height: '24px',
+      viewBox: '0 0 24 24',
+      version: '1.1',
+      xmlns: 'http://www.w3.org/2000/svg'
+    },
+    React.createElement(
+      'title',
+      null,
+      'icons/general/book'
+    ),
+    React.createElement(
+      'desc',
+      null,
+      'Created with Sketch.'
+    ),
+    React.createElement(
+      'g',
+      { id: 'icons/general/book', stroke: 'none', strokeWidth: '1', fill: 'none', fillRule: 'evenodd' },
+      React.createElement(
+        'g',
+        { id: 'Icon', fill: '#FFFFFF', fillOpacity: '0' },
+        React.createElement('rect', { id: 'Margin', x: '0', y: '0', width: '24', height: '24' })
+      ),
+      React.createElement('path', {
+        d: 'M19,2 L14,6.5 L14,17.5 L19,13 L19,2 L19,2 Z M6.5,5 C4.55,5 2.45,5.4 1,6.5 L1,21.16 C1,21.41 1.25,21.66 1.5,21.66 C1.6,21.66 1.65,21.59 1.75,21.59 C3.1,20.94 5.05,20.5 6.5,20.5 C8.45,20.5 10.55,20.9 12,22 C13.35,21.15 15.8,20.5 17.5,20.5 C19.15,20.5 20.85,20.81 22.25,21.56 C22.35,21.61 22.4,21.59 22.5,21.59 C22.75,21.59 23,21.34 23,21.09 L23,6.5 C22.4,6.05 21.75,5.75 21,5.5 L21,7.5 L21,13 L21,19 C19.9,18.65 18.7,18.5 17.5,18.5 C15.8,18.5 13.35,19.15 12,20 L12,13 L12,8.5 L12,6.5 C10.55,5.4 8.45,5 6.5,5 L6.5,5 Z',
+        id: 'Shape',
+        fill: '#575451'
+      })
+    )
+  );
+};
+
+BookIcon.propTypes = {
+  className: PropTypes.string
+};
+
 var ColorPicker$1 = function (_Component) {
   inherits(ColorPicker, _Component);
   createClass(ColorPicker, null, [{
@@ -36161,5 +36205,5 @@ Drawer$1.defaultProps = {
   placement: 'right'
 };
 
-export { AutoComplete$1 as AutoComplete, Button$1 as Button, Checkbox$1 as Checkbox, Chips, DatePicker$1 as DatePicker, Dropdown$1 as Dropdown, Input$1 as Input, TextArea, MultiSelect, Select$1 as Select, ShipmentModeIcon, SubHeader, Radio$1 as Radio, DateRange, StickyNav, Popover$1 as Popover, ColorPicker$1 as ColorPicker, Layers, Settings, Checkmark, Close, ChevronDown, Link, Info, Copy, colors, MultiInput, ThemeProvider, ChevronRight, DocumentIcon, Drawer$1 as Drawer, NoteIcon };
+export { AutoComplete$1 as AutoComplete, Button$1 as Button, Checkbox$1 as Checkbox, Chips, DatePicker$1 as DatePicker, Dropdown$1 as Dropdown, Input$1 as Input, TextArea, MultiSelect, Select$1 as Select, ShipmentModeIcon, SubHeader, Radio$1 as Radio, DateRange, StickyNav, Popover$1 as Popover, ColorPicker$1 as ColorPicker, Layers, Settings, Checkmark, Close, ChevronDown, Link, Info, Copy, colors, MultiInput, ThemeProvider, ChevronRight, DocumentIcon, Drawer$1 as Drawer, NoteIcon, BookIcon };
 //# sourceMappingURL=index.es.js.map
--- dist/index.js
+++ dist/index.js
@@ -35602,6 +35602,50 @@ NoteIcon.propTypes = {
   className: PropTypes.string
 };
 
+var BookIcon = function BookIcon(_ref) {
+  var className = _ref.className;
+
+  return React__default.createElement(
+    'svg',
+    {
+      className: className,
+      width: '24px',
+      height: '24px',
+      viewBox: '0 0 24 24',
+      version: '1.1',
+      xmlns: 'http://www.w3.org/2000/svg'
+    },
+    React__default.createElement(
+      'title',
+      null,
+      'icons/general/book'
+    ),
+    React__default.createElement(
+      'desc',
+      null,
+      'Created with Sketch.'
+    ),
+    React__default.createElement(
+      'g',
+      { id: 'icons/general/book', stroke: 'none', strokeWidth: '1', fill: 'none', fillRule: 'evenodd' },
+      React__default.createElement(
+        'g',
+        { id: 'Icon', fill: '#FFFFFF', fillOpacity: '0' },
+        React__default.createElement('rect', { id: 'Margin', x: '0', y: '0', width: '24', height: '24' })
+      ),
+      React__default.createElement('path', {
+        d: 'M19,2 L14,6.5 L14,17.5 L19,13 L19,2 L19,2 Z M6.5,5 C4.55,5 2.45,5.4 1,6.5 L1,21.16 C1,21.41 1.25,21.66 1.5,21.66 C1.6,21.66 1.65,21.59 1.75,21.59 C3.1,20.94 5.05,20.5 6.5,20.5 C8.45,20.5 10.55,20.9 12,22 C13.35,21.15 15.8,20.5 17.5,20.5 C19.15,20.5 20.85,20.81 22.25,21.56 C22.35,21.61 22.4,21.59 22.5,21.59 C22.75,21.59 23,21.34 23,21.09 L23,6.5 C22.4,6.05 21.75,5.75 21,5.5 L21,7.5 L21,13 L21,19 C19.9,18.65 18.7,18.5 17.5,18.5 C15.8,18.5 13.35,19.15 12,20 L12,13 L12,8.5 L12,6.5 C10.55,5.4 8.45,5 6.5,5 L6.5,5 Z',
+        id: 'Shape',
+        fill: '#575451'
+      })
+    )
+  );
+};
+
+BookIcon.propTypes = {
+  className: PropTypes.string
+};
+
 var ColorPicker$1 = function (_Component) {
   inherits(ColorPicker, _Component);
   createClass(ColorPicker, null, [{
@@ -36204,4 +36248,5 @@ exports.ChevronRight = ChevronRight;
 exports.DocumentIcon = DocumentIcon;
 exports.Drawer = Drawer;
 exports.NoteIcon = NoteIcon;
+exports.BookIcon = BookIcon;
 //# sourceMappingURL=index.js.map
