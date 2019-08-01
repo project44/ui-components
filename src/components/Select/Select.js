9962909d94be8e946cbad54bed7e882ff7bf056c
kgourwarp44 <47984312+kgourwarp44@users.noreply.github.com>
2019-08-01 16:23:59 -0500

    refactor select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -3,6 +3,18 @@ import PropTypes from 'prop-types';
 import { Select as AntSelect } from 'antd';
 import _ from 'lodash';
 import './Select.scss';
+import styled from 'styled-components';
+import { rgba } from 'polished';
+
+import { ThemeContext, defaultThemeShape } from '../../styles/theme';
+
+const StyledSelect = styled.div`
+  .ant-select-selection:hover,
+  .ant-select-selection:focus {
+    border: 1px solid ${props => props.theme.primaryColor};
+    box-shadow: 0 0 0 2px ${props => rgba(props.theme.primaryColor, 0.2)};
+  }
+`;
 
 export default class Select extends Component {
   static propTypes = {
@@ -13,35 +25,47 @@ export default class Select extends Component {
     showSearch: PropTypes.bool,
     defaultValue: PropTypes.string,
     onChange: PropTypes.func,
-    icon: PropTypes.string
-  }
+    icon: PropTypes.string,
+    theme: PropTypes.shape(defaultThemeShape),
+  };
+  static contextType = ThemeContext;
 
   constructor(props) {
     super(props);
     this.state = {
-      options: _.get(props, 'dataSource', [])
+      options: _.get(props, 'dataSource', []),
     };
   }
 
   handleDropdownChange = () => {
     this.setState({
-      open: !this.state.open
+      open: !this.state.open,
     });
-  }
+  };
 
   render() {
-    const { mode = 'default', label, placeholder, custom, showSearch, defaultValue, onChange, icon = 'arrow_drop_down', ...props } = this.props;
+    const {
+      mode = 'default',
+      label,
+      placeholder,
+      custom,
+      showSearch,
+      defaultValue,
+      onChange,
+      icon = 'arrow_drop_down',
+      ...props
+    } = this.props;
     return (
-      <div className='select ant-form-vertical'>
-        { label &&
-        <div className='ant-form-item-label'>
-          <label title={label}>{label}</label>
-        </div>
-        }
-        <div className='select__dropdown'>
+      <StyledSelect theme={this.props.theme || this.context} className="select ant-form-vertical">
+        {label && (
+          <div className="ant-form-item-label">
+            <label title={label}>{label}</label>
+          </div>
+        )}
+        <div className="select__dropdown">
           <AntSelect
             showSearch={showSearch}
-            className='select__input'
+            className="select__input"
             showArrow={false}
             onDropdownVisibleChange={this.handleDropdownChange}
             placeholder={placeholder}
@@ -51,15 +75,15 @@ export default class Select extends Component {
             {...custom}
             {...props}
           >
-            {this.state.options.map((item) => {
+            {this.props.dataSource.map(item => {
               return <AntSelect.Option key={item.id}>{item.value}</AntSelect.Option>;
             })}
           </AntSelect>
-          <div className='select__icon'>
+          <div className="select__icon">
             <i className={`material-icons ${this.state.open ? 'open' : 'closed'}`}>{icon}</i>
           </div>
         </div>
-      </div>
+      </StyledSelect>
     );
   }
 }
